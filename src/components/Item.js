068079efa5de460768068d3357bd655aff10bcb7
AK <akueiminyang@cmail.carleton.ca>
2023-06-03 17:39:00 -0400

    Lab completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  const handleItemToggle = () => {
+    setInCart(!inCart);
+  };
+
+  const itemClass = inCart ? "in-cart" : "";
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart";
+  const buttonClassName = inCart ? "remove" : "add";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClassName} onClick={handleItemToggle}>
+        {buttonText}
+      </button>
     </li>
   );
 }
